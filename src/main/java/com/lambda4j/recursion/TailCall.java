d9ad033caa36d78001d9c161ffe241dcffeb8a43
alexalbu98 <alexalbu78@yahoo.com>
2023-08-28 15:19:50 +0300

    Implemented tailcall.
    Signed-off-by: alexalbu98 <alexalbu78@yahoo.com>

---
+++ src/main/java/com/lambda4j/recursion/TailCall.java
@@ -0,0 +1,65 @@
+package com.lambda4j.recursion;
+
+import com.lambda4j.function.Supplier;
+
+public abstract class TailCall<T> {
+    public abstract TailCall<T> resume();
+
+    public abstract T eval();
+
+    public abstract boolean isSuspend();
+
+    public static <T> Return<T> ret(T t) {
+        return new Return<>(t);
+    }
+
+    public static <T> Suspend<T> sus(Supplier<TailCall<T>> s) {
+        return new Suspend<>(s);
+    }
+
+    public static class Return<T> extends TailCall<T> {
+        private final T t;
+
+        public Return(T t) {
+            this.t = t;
+        }
+
+        @Override
+        public TailCall<T> resume() {
+            throw new IllegalStateException("Return has no resume");
+        }
+
+        @Override
+        public T eval() {
+            return t;
+        }
+
+        @Override
+        public boolean isSuspend() {
+            return false;
+        }
+    }
+
+    public static class Suspend<T> extends TailCall<T> {
+        private final Supplier<TailCall<T>> resume;
+
+        private Suspend(Supplier<TailCall<T>> resume) {
+            this.resume = resume;
+        }
+
+        @Override
+        public TailCall<T> resume() {
+            return resume.get();
+        }
+
+        @Override
+        public T eval() {
+            throw new IllegalStateException("Suspend has no value");
+        }
+
+        @Override
+        public boolean isSuspend() {
+            return true;
+        }
+    }
+}
