899bda1c776973f2f5ed57d90531d6abbf5d62c6
alexalbu98 <alexalbu78@yahoo.com>
2023-08-28 09:28:23 +0300

    Added generics to functions.
    Signed-off-by: alexalbu98 <alexalbu78@yahoo.com>

--- src/main/java/com/lambda4j/function/Function.java
+++ src/main/java/com/lambda4j/function/Function.java
@@ -3,11 +3,11 @@ package com.lambda4j.function;
 public interface Function<T, U> {
     U apply(T arg);
 
-    default <V> Function<V, U> compose(Function<V, T> f) {
+    default <V> Function<V, U> compose(Function<? super V, ? extends T> f) {
         return x -> apply(f.apply(x));
     }
 
-    static <T, U, V> Function<V, U> compose(Function<T, U> f, Function<V, T> g) {
+    static <T, U, V> Function<V, U> compose(Function<? super T, ? extends U> f, Function<? super V, ? extends T> g) {
         return x -> f.apply(g.apply(x));
     }
 
@@ -16,21 +16,20 @@ public interface Function<T, U> {
     }
 
     static <T, U, V> Function<Function<U, V>, Function<Function<T, U>, Function<T, V>>> higherCompose() {
-        return (Function<U, V> x) ->
-                (Function<T, U> y) -> (T z) -> x.apply(y.apply(z));
+        return (Function<U, V> x) -> (Function<T, U> y) -> (T z) -> x.apply(y.apply(z));
     }
 
 
-    default <V> Function<T, V> andThen(Function<U, V> f) {
+    default <V> Function<T, V> andThen(Function<? super U, ? extends V> f) {
         return x -> f.apply(apply(x));
+    }
+
+    static <T, U, V> Function<T, V> andThen(Function<? super T, ? extends U> f, Function<? super U, ? extends V> g) {
+        return x -> g.apply(f.apply(x));
     }
 
     static <T, U, V> Function<Function<T, U>, Function<Function<V, T>, Function<V, U>>> andThen() {
         return x -> y -> y.andThen(x);
-    }
-
-    static <T, U, V> Function<T, V> andThen(Function<T, U> f, Function<U, V> g) {
-        return x -> g.apply(f.apply(x));
     }
 
     static <T, U, V> Function<Function<T, U>, Function<Function<U, V>, Function<T, V>>> higherAndThen() {
