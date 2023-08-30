f7b4a05906941ae750d8462724887c37cbb60032
alexalbu98 <alexalbu78@yahoo.com>
2023-08-30 12:01:37 +0300

    Created filter for option.
    Signed-off-by: alexalbu98 <alexalbu78@yahoo.com>

--- src/main/java/com/lambda4j/option/Option.java
+++ src/main/java/com/lambda4j/option/Option.java
@@ -15,6 +15,16 @@ public abstract class Option<A> {
 
     public abstract <B> Option<B> flatMap(Function<? super A, Option<B>> f);
 
+    public Option<A> orElse(Supplier<Option<A>> defaultValue) {
+        return map(x -> this).getOrElse(defaultValue);
+    }
+
+    public Option<A> filter(Function<A, Boolean> f) {
+        return flatMap(x -> f.apply(x)
+                ? this
+                : none());
+    }
+
     private static class None<A> extends Option<A> {
         private None() {
         }
--- src/test/java/com/lambda4j/OptionTests.java
+++ src/test/java/com/lambda4j/OptionTests.java
@@ -3,6 +3,7 @@ package com.lambda4j;
 import com.lambda4j.option.Option;
 import org.junit.jupiter.api.Test;
 
+import static com.lambda4j.option.Option.none;
 import static com.lambda4j.option.Option.some;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -31,5 +32,19 @@ class OptionTests {
         Option<Integer> option = some(1);
         option = option.flatMap(i -> some(i + 1));
         assertEquals(2, option.getOrElse(() -> 0));
+    }
+
+    @Test
+    void or_else_works() {
+        Option<Integer> option = none();
+        option = option.orElse(() -> some(2));
+        assertEquals(2, option.getOrElse(() -> 0));
+    }
+
+    @Test
+    void filter_works() {
+        Option<Integer> option = some(1);
+        option = option.filter(x -> x != 1);
+        assertEquals(0, option.getOrElse(() -> 0));
     }
 }
