8eb79133adcbd324cb7a96fe1c1262a9c9086075
Albu Alexandru <alexalbu78@yahoo.com>
2023-09-03 15:03:39 +0300

    Added test for sequence state method.
    Signed-off-by: Albu Alexandru <alexalbu78@yahoo.com>

--- src/test/java/com/lambda4j/StateTests.java
+++ src/test/java/com/lambda4j/StateTests.java
@@ -1,5 +1,6 @@
 package com.lambda4j;
 
+import com.lambda4j.list.List;
 import com.lambda4j.random.JavaRNG;
 import com.lambda4j.random.RNG;
 import com.lambda4j.state.Nothing;
@@ -7,6 +8,7 @@ import com.lambda4j.state.State;
 import com.lambda4j.tuple.Tuple;
 import org.junit.jupiter.api.Test;
 
+import static com.lambda4j.list.List.list;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StateTests {
@@ -49,11 +51,14 @@ public class StateTests {
 
     @Test
     public void sequence_works() {
-        State<RNG, Integer> state = State.unit(1);
-        state = state.flatMap(i -> State.unit(i + 1));
+        State<RNG, Integer> state1 = State.unit(1);
+        State<RNG, Integer> state2 = State.unit(2);
 
-        Tuple<Integer, RNG> result = state.run.apply(rng);
-        assertEquals(2, result.first);
+        State<RNG, List<Integer>> state = State.sequence(list(state1, state2));
+        Tuple<List<Integer>, RNG> tuple = state.run.apply(rng);
+
+        assertEquals(2, tuple.first.lengthMemoized());
+        assertEquals(1, tuple.first.head());
     }
 
     @Test
